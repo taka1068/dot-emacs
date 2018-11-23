246908fdbaa568a21b4acfb89e8d4a226d6fbfd7
TakanoriHirobe <london.2xxx@gmail.com>
2018-11-23 21:53:56 +0900

    added some mac like key bindings

--- home/.emacs.d/conf/10-keybindings.el
+++ home/.emacs.d/conf/10-keybindings.el
@@ -4,3 +4,12 @@
 
 ;; "C-t" to go to other window
 (define-key global-map (kbd "C-t") 'other-window)
+
+;; "s-w" to close window
+(define-key global-map (kbd "s-w") 'delete-window)
+
+;; "s-t" to split window horizontally
+(define-key global-map (kbd "s-t") 'split-window-right)
+
+;; "M-t" to split window vertically
+(define-key global-map (kbd "M-t") 'split-window-below)
