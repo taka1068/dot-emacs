347bb1c9377c4f2516834adf3948ebcaca1a06b1
TakanoriHirobe <london.2xxx@gmail.com>
2019-03-16 23:09:02 +0900

    remove some cua mode like keybindings

--- home/.emacs.d/conf/10-keybindings.el
+++ home/.emacs.d/conf/10-keybindings.el
@@ -5,27 +5,6 @@
 ;; "C-t" to go to other window
 (define-key global-map (kbd "C-t") 'other-window)
 
-;; "s-w" to close window
-(define-key global-map (kbd "s-w") 'delete-window)
-
-;; "s-t" to split window horizontally
-(define-key global-map (kbd "s-t") 'split-window-right)
-
-;; "M-t" to split window vertically
-(define-key global-map (kbd "M-t") 'split-window-below)
-
-;; "s-c" to copy
-(define-key global-map (kbd "s-c") 'kill-ring-save)
-
-;; "s-v" to yank
-(define-key global-map (kbd "s-v") 'yank)
-
-;; "s-x" to cut
-(define-key global-map (kbd "s-x") 'kill-region)
-
-;; "s-z" to undo
-(define-key global-map (kbd "s-z") 'undo)
-
 (define-key global-map (kbd "s-{") 'previous-buffer)
 
 (define-key global-map (kbd "s-}") 'next-buffer)
