a6c421258a13a0df86ae5baa7f3923543421161a
TakanoriHirobe <london.2xxx@gmail.com>
2019-12-30 01:03:54 +0900

    install rust-mode

--- home/.emacs.d/init.el
+++ home/.emacs.d/init.el
@@ -3,12 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
  '(custom-safe-themes
    (quote
     ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
  '(package-selected-packages
    (quote
-    (markdown-mode color-theme-sanityinc-tomorrow spacemacs-theme auto-complete swift-mode init-loader))))
+    (rust-mode markdown-mode color-theme-sanityinc-tomorrow spacemacs-theme auto-complete swift-mode init-loader))))
 
 ;; add to load path recursively
 (defun add-to-load-path (&rest paths)
